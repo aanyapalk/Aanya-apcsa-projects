2f4b68688313cceb15080b3c96d0b876b5026681
aanyapalk <apalkonda@gmail.com>
2025-09-15 20:20:21 +0000

    Roomba correct code

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,126 +4,101 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-	// Main method to make this self-contained
-	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
-		String worldName = "robot/TestRoom.wld";
+    public static void main(String[] args) {
+        // LEAVE THIS ALONE!!!!!!
+        String worldName = "robot/TestRoom.wld";
+        World.setDelay(0);
+        Roomba cleaner = new Roomba();
+        int totalBeepers = cleaner.cleanRoom(worldName, 25, 11);
+        //System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");  
+        //System.out.println("Roomba cleaned up a total of " + totalBeepers + " piles.");
+    }
 
-		Roomba cleaner = new Roomba();
-		int totalBeepers = cleaner.cleanRoom(worldName, 25, 13);
-		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
+    // declared here so it is visible in all the methods!
+    private Robot roomba;
 
-	}
+    public int cleanRoom(String worldName, int startX, int startY) {
 
-	// declared here so it is visible in all the methods!
-	private Robot roomba = new Robot(25,13,East,0);
-
-
-	// You will need to add many variables!!
-
-
-	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
+        // A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
         // Make sure it starts at startX and startY location.
 
-		World.readWorld(worldName);
-		World.setVisible(true);
-		World.setDelay(0);
+        World.readWorld(worldName);
+        World.setVisible(true);
+        boolean end = true;
+        roomba = new Robot(startX, startY, East, 0); 
+        int numOfBeepers = 0; 
+        int units = 0; 
+        int numOfPiles = 0;  
+        int pileOfMaxBeepers = 0; 
+        int street = roomba.street();   
+        int ave = roomba.avenue();      
 
+        while(end){
+            while(roomba.frontIsClear()){
+                roomba.move(); 
+                units++; 
+                int max2 = 0;
+                if(roomba.nextToABeeper() == true){
 
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
+                    numOfPiles++; 
+                    int currentPileSize = 0;
+                    while(roomba.nextToABeeper()){
+                        roomba.pickBeeper(); 
+                        numOfBeepers++; 
+                        max2 += 1;
+                        if(max2 >= pileOfMaxBeepers){
+                            pileOfMaxBeepers = max2;
+                            street = roomba.street(); 
+                            ave = roomba.avenue(); 
+                        }
+                    } 
+                }
+            }
+            if(roomba.facingEast() && !roomba.frontIsClear()){
+                roomba.turnLeft(); 
+                if(!roomba.frontIsClear()){
+                    units++;
+                    end = false;
+                }
+                else{
+                    roomba.move();
+                    units++; 
+                    roomba.turnLeft(); 
+                }
+                ; 
+            }
+            else if(roomba.facingWest() && !roomba.frontIsClear()){
+                turnRight(roomba); 
+                if(!roomba.frontIsClear()){
+                    units++; 
+                    end = false; 
+                }else{
+                roomba.move(); 
+                units++;  
+                turnRight(roomba); 
+                }
+            }
+        } 
 
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-		
-int largestPile = 0;
-int totalPiles = 0;
-int beepers;
-int largestX = startX;
-int largestY = startY;
-int totalBeepers = 0;
-for(int i= 0; i<14; i++)
-{
-while (roomba.frontIsClear())
-{
-beepers = 0;
-while (roomba.nextToABeeper())
-{
-	roomba.pickBeeper();
-	beepers++;
-	totalBeepers++;
-}
-if (beepers > 0){
-	totalPiles++;
-}
-if (beepers > largestPile){
-	largestPile = beepers;
-	largestX = roomba.street();
-	largestY = roomba.avenue();
-} 
-	roomba.move();
-}
-roomba.turnLeft();
-roomba.move();
-roomba.turnLeft();
-while (roomba.frontIsClear())
-{
-beepers = 0;
-while (roomba.nextToABeeper())
-{
-	roomba.pickBeeper();
-	beepers++;
-	totalBeepers++;
-}
-if (beepers > 0){
-	totalPiles++;
-}
-if (beepers > largestPile){
-	largestPile = beepers;
-	largestX = roomba.street();
-	largestY = roomba.avenue();
-} 
-	roomba.move();
-}
-roomba.turnLeft();
-roomba.turnLeft();
-roomba.turnLeft();
-roomba.move();
-roomba.turnLeft();
-roomba.turnLeft();
-roomba.turnLeft();
-while (roomba.frontIsClear())
-{
-beepers = 0;
-while (roomba.nextToABeeper())
-{
-	roomba.pickBeeper();
-	beepers++;
-	totalBeepers++;
-}
-if (beepers >0){
-	totalPiles++;
-}
-if (beepers > largestPile){
-	largestPile = beepers;
-	largestX = roomba.street();
-	largestY = roomba.avenue();
-} 
-	roomba.move();
-}
-{
-	System.out.print("The most amount of beepers in a pile is ");
-	System.out.println(largestPile);
-	System.out.print("The total amount of piles is ");
-	System.out.println(totalPiles);
-	System.out.print("The coordinate of the largest pile is ");
-	System.out.println(largestX  + "," + largestY );
-}
-}
-		return totalBeepers;
-	}
+        double averagePileSize = (double)numOfBeepers/numOfPiles ; 
+        double percentDirty = (double)numOfPiles / units; 
+
+        
+        System.out.println("Total number of Beepers = "+ numOfBeepers);
+        System.out.println("Total number of Piles = "+ numOfPiles); 
+        System.out.println("Total Squares traveled: "+units); 
+        System.out.println("The maximum number of beepers in a pile is "+pileOfMaxBeepers); 
+        System.out.println("Coordinates: ("+street+ ", "+ave+")"); 
+        //System.out.println("Relative Coordinates: ("+(street-startX)+", "+(ave - startY)+")");
+        System.out.println("Average Pile size = "+ averagePileSize); 
+        System.out.println("Percent dirty: "+percentDirty); 
+
+        return numOfPiles; 
+    }
+
+    public static void turnRight(Robot roomba){
+        roomba.turnLeft(); 
+        roomba.turnLeft(); 
+        roomba.turnLeft(); 
+    }
 }
