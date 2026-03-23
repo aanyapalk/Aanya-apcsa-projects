d701087365753163e60752b36bfc343c23afd17c
aanyapalk <apalkonda@gmail.com>
2026-03-23 20:53:42 +0000

    Tower Day 1

--- polygon/Main.java
+++ polygon/Main.java
@@ -1,12 +1,17 @@
 package polygon;
-
+import java.awt.geom.*;
 public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+        myPolygon.add (new Point2D.Double(0,0));
+        myPolygon.add (new Point2D.Double(100,50));
+        myPolygon.add (new Point2D.Double(100,0));
+        myPolygon.add (new Point2D.Double(0,50));
         myPolygon.draw();
 
         TestSuite.run();
+
     }
     
 }
